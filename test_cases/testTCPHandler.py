1518be3ff0c9c3268856cd1eb400fde7a9be6335
Oleksandr Khaustovych <oleksandr.khaustovych@wesoftyou.com>
2023-06-08 11:11:23 +0300

    new tests

--- test_cases/testTCPHandler.py
+++ test_cases/testTCPHandler.py
@@ -20,6 +20,18 @@ class Test_case_Server(unittest.TestCase):
             self.assertEqual(thread.is_alive(), False, 'not all threads stopped')
 
 
+ADDRESS_DEFAULT = (1, 2, 3)
+PLAYER_ID_DEFAULT = 23
+
+SERVER_ADDRESS = ('', 9080)
+BACKLOG = 10
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_create_tcp_server(self):
+        tcp_server = TCPHandler.tcp_server(SERVER_ADDRESS, BACKLOG)
+        self.assertEqual(str(type(tcp_server)), "<class 'socket.socket'>", 'incorrect return type')
 
 
 if __name__ == '__main__':
